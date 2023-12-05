ebaf0a15fa76f3ea81045f76b7a1cf1d4941e186
Kredao <Kredao2007@gmail.com>
2023-12-05 16:40:10 +0600

    добавлена функция для отображения дня недели

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -5,13 +5,28 @@ import React from 'react'
 
 export default function HeaderItem({ item, activeDay, setActiveDay }) {
 
+    function convertDay(day) {
+        const dateParts = day.split('.');
+        const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+    
+        const newDate = new Date(formattedDate);
+        if (isNaN(newDate)) {
+            return `Некорректная дата`; // Обработка ошибки парсинга даты
+        }
+    
+        const list = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        const dayIndex = newDate.getDay(); // Получение номера дня недели
+    
+        return list[dayIndex];
+    }
+
     return (
         <Pressable style={() => [{
                     borderColor: (activeDay === item.name) ? color.main : color.mainTransparent,
                 }, styles.container]} onPress={() => setActiveDay(item.name)}>
             <Text style={{
                 color: (activeDay == item.name) ? color.main : color.mainTransparent,
-            }}>{`${item.name}`}</Text>
+            }}>{`${convertDay(item.name)}, ${item.name}`}</Text>
         </Pressable>
     )
 }
--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-let data;
-
 function loadGroups(id) {
-    if (id !== undefined) {
-        fetch(`https://schedule-backend-production.koka.team/v1/schedule?group_id=${id}&is_new=true`)
-            .then(response => response.json())
-            .then(response => {
-                data = JSON.parse(JSON.stringify(response.schedule));
-                if (typeof data === 'object' && data !== null && Object.keys(data).length > 0) {
-                    console.log('Данные успешно загружены');
-                    console.log(data["02.12.2023"].schedule); // Вывод данных здесь, после загрузки
-                } else {
-                    console.log('Данные не загружены');
-                }
-            })
-            .catch(error => {
-                console.error('Произошла ошибка при загрузке данных:', error);
-            });
-    }
+    let date = id.split('.');
+    date = `${date[2]}.${date[1]}.${date[0]}`
+    date = new Date(Date.parse(date)).getDay()
+
+
+    const list = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб']
+    return list[date]
 }
 
-loadGroups(236);
+console.log(loadGroups('05.12.2023'))
